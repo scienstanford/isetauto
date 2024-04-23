542bf0e485061a46ee59188e728d31bc62ab1fd9
Brian Wandell <1837145+wandell@users.noreply.github.com>
2024-04-22 18:13:57 -0700

    Update s_autoLightGroups.m

--- scripts/lightgroups/s_autoLightGroups.m
+++ scripts/lightgroups/s_autoLightGroups.m
@@ -21,7 +21,15 @@ host = 'orange.stanford.edu';
 
 % Prepare the local directory
 imageID = '1114091636';
+% 1114091636 - People on street
+% 1114011756 - Vans moving away, person
+% 1113094429
+
+lgt = {'headlights','streetlights','otherlights','skymap'};
 destPath = fullfile(iaRootPath,'local',imageID);
+
+%% Download the four light group EXR files and make them into scenes
+
 if ~exist(destPath,'dir'), mkdir(destPath); end
 
 % First the metadata
@@ -30,41 +38,24 @@ src  = fullfile(metaFolder,[imageID,'.mat']);
 ieSCP(user,host,src,destPath);
 load(fullfile(destPath,[imageID,'.mat']),'sceneMeta');
 
-% Now the four light group EXR files
-lgt = {'headlights','streetlights','otherlights','skymap'};
-destFile = cell(4,1);
-for ll = 1:numel(lgt)
-    thisFile = sprintf('%s_%s.exr',imageID,lgt{ll});
-    srcFile  = fullfile(sceneMeta.datasetFolder,thisFile);
-    destFile{ii} = fullfile(destPath,thisFile);
-    ieSCP(user,host,srcFile,destFile{ii});
-end
-
-%% We have the files.  Make some images
-
-parameters.fnumber = 1.7;
-parameters.focallength = 4.38e-3;
-parameters.nsides = 20;
- 
-% IMX353 sensor tends to make the saturated pixels pink.
-parameters.sensormodel = 'ar0132at';
-parameters.pixelsize = 1.4e-6;
-parameters.analoggain = 1/5; % 3 times
-parameters.exposuretime = 1/60;
-
-
-load(fullfile(destPath, [imageID, '.mat']),'sceneMeta');
-
-% Now the four light group EXR files
-lgt = {'headlights','streetlights','otherlights','skymap'};
-scenes = cell(numel(lgt,1));
+% The the radiance EXR files
 for ll = 1:numel(lgt)
     thisFile = sprintf('%s_%s.exr',imageID,lgt{ll});
     srcFile  = fullfile(sceneMeta.datasetFolder,thisFile);
     destFile = fullfile(destPath,thisFile);
     ieSCP(user,host,srcFile,destFile);
+end
+
+%% Load up the scenes from the downloaded directory
+
+scenes = cell(numel(lgt,1));
+for ll = 1:numel(lgt)
+    thisFile = sprintf('%s_%s.exr',imageID,lgt{ll});
+    destFile = fullfile(destPath,thisFile);
     scenes{ll} = piEXR2ISET(destFile);
 end
+
+%% Combine the scenes
 
 %% Just show the point lights
 wgts = [1 1 1 0];
@@ -86,35 +77,61 @@ scene.metadata.wgts = wgts;
 scene = piAIdenoise(scene);
 sceneWindow(scene);
 
-%% 
+%%  Combine them into a merged radiance scene
+
 wgts = [0.1, 0.1, 0.02, 0.001]; % night
 scene = sceneAdd(scenes, wgts);
 scene.metadata.wgts = wgts;
 scene = piAIdenoise(scene);
-% sceneWindow(scene);
-% scene = sceneSet(scene,'render flag','hdr');
-% scene = sceneSet(scene,'gamma',2.1);
+sceneWindow(scene);
+scene = sceneSet(scene,'render flag','hdr');
+scene = sceneSet(scene,'gamma',2.1);
+
 %{
  lum = sceneGet(scene,'luminance');
  ieNewGraphWin; mesh(log10(lum));
 %}
 %% We could convert the scene via wvf in various ways
 
-% [aperture, params] = wvfAperture(wvf,'nsides',0,...
-%     'dot mean',50, 'dot sd',20, 'dot opacity',0.5,'dot radius',5,...
-%     'line mean',50, 'line sd', 20, 'line opacity',0.5,'linewidth',2);
-oi = oiCreate;
-oi = oiCompute(oi, scene);
-oi = oiCrop(oi,'border');
+wvf = wvfCreate;
+[aperture, params] = wvfAperture(wvf,'nsides',3,...
+    'dot mean',50, 'dot sd',20, 'dot opacity',0.5,'dot radius',5,...
+    'line mean',50, 'line sd', 20, 'line opacity',0.5,'linewidth',2);
+
+oi = oiCreate('wvf');
+oi = oiCompute(oi, scene,'aperture',aperture,'crop',true);
+% oiWindow(oi);
 
 %
-ip = piRadiance2RGB(oi,'etime',1/30,'analoggain',1/10);
-
+[ip, sensor] = piRadiance2RGB(oi,'etime',1/30,'analoggain',1/10);
 % sensor = sensorSet(sensor, 'pixel size', 4e-6);
 % sensor = sensorSet(sensor, 'exposure time', 1/30);
 % sensor = sensorCompute(sensor, oi);
 % ip = ipCompute(ip, sensor);
  
-ipWindow(ip)
+ipWindow(ip);
+
+
+%%
+sensor = sensorSet(sensor,'noiseFlag',0);
+sensor = sensorSet(sensor,'name','noise free');
+sensor = sensorCompute(sensor,oi);
+ip = ipCompute(ip,sensor);
+ipWindow(ip);
+
+%%  The RGB filters differ a lot and thus the rendering differs
+
+sensor2 = sensorCreate('rgbw');
+sensor2 = sensorSet(sensor2,'size',sensorGet(sensor,'size'));
+sensor2 = sensorSet(sensor2,'name','rgbw');
+sensor2 = sensorSet(sensor2,'pixel size constant fill factor',[1.4 1.4]*1e-6);
+sensor2 = sensorSet(sensor2,'fov',sensorGet(sensor,'fov'),oi);
+sensor2 = sensorSet(sensor2,'exp time',0.033);
+sensor2 = sensorCompute(sensor2,oi);
+% sensorWindow(sensor2);
+
+ip = ipCompute(ip,sensor2);
+ip = ipSet(ip,'name',sensorGet(sensor2,'name'));
+ipWindow(ip);
 
 %%
